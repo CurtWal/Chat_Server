37f4f5ce71801480e09ca427e2dba8144bc72777
Curtrick Walton <curtrickw@yahoo.com>
2022-09-20 09:54:56 -0500

    first steps

--- routes/msg.js
+++
@@ -1,7 +0,0 @@
-const { addMessage, getMessages } = require("../controllers/msgController");
-const router = require("express").Router();
-
-router.post("/addmsg/", addMessage);
-router.post("/getmsg/", getMessages);
-
-module.exports = router;
